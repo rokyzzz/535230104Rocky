2e5191f23352403f5760a88bb919d3b106f75ad8
usernamerokyzzz <emailmuhammad.535230104@stu.untar.ac.id>
2024-09-02 18:12:37 +0700

    final commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-// Toggle & Responsive Navigation
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const navLists = document.querySelector("nav");
 
   burger.addEventListener("click", () => {
-    // Toggle nav list and burger class
     navLists.classList.toggle("nav-active");
     burger.classList.toggle("toggle-burger");
   });
@@ -12,7 +10,7 @@ const navSlide = () => {
 
 navSlide();
 
-// Clear form before unload
+
 window.onbeforeunload = () => {
   for (const form of document.getElementsByTagName("form")) {
     form.reset();
